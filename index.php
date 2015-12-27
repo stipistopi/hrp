86a83a0d041b7e59ab27c313545221db5c7a87df
Peter Tisoczki <Tisoczki.Peter.Mark@stud.u-szeged.hu>
2015-12-27 23:07:30 +0100

    Index.php created

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>HRP Interaktív Program</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <!--
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    -->
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <!--
+    <script type="text/javascript" src=""></script>
+    <script type="text/javascript">
+    </script>
+    -->
+</head>
+
+<body>
+    <p>Teszt</p>
+    <div class="nagyKor kozepre"></div>
+</body>
+</html>
