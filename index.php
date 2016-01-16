de73beb7593f4d5a46735c15126ff979db09c8e0
Peter Tisoczki <Tisoczki.Peter.Mark@stud.u-szeged.hu>
2016-01-16 03:58:28 +0100

    First page is done

--- index.php
+++ index.php
@@ -2,30 +2,45 @@
 <html>
 <head>
     <title>HRP Interaktív Program</title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
     <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="css/style.css">
     <script src="js/bootstrap.min.js"></script>
     <script src="js/jquery.min.js"></script>
     <script src="js/main.js"></script>
     <script type="text/javascript">
-        $(document).ready(function() {
+        /*$(document).ready(function() {
             $(".kisKor").hover(function() {
                 $(".kisKor").css("animation-play-state", "paused");
             }, function() {
                 $(".kisKor").css("animation-play-state", "running");
             });
-        });
+        });*/
     </script>
 </head>
 
 <body>
-    <!--
-    <div class="nagyKor kozepre"></div>
-    -->
-    <a href="#"><div class="kisKor kisKor1"><span class="szoveg">Bemutatkozó</span></div></a>
-    <a href="#"><div class="kisKor kisKor2"><span class="szoveg">Programajánló demó</span></div></a>
-    <a href="#"><div class="kisKor kisKor3"><span class="szoveg">Interaktív program indítása</span></div></a>
+    <img class="logo img-responsive" src="kepek/logo.png" alt="HRP logó" width="30%" height="auto">
+    <div class="container-fluid bentebb">
+        <div class="row">
+            <div class="col-xs-12 col-md-4">
+                <div class="responsive-circle kisKor1">
+                    <a href="#"><div class="circle-inner"><span class="szoveg">Rólunk</span></div></a>
+                </div>
+            </div>
+            <div class="col-xs-12 col-md-4">
+                <div class="responsive-circle kisKor2">
+                    <a href="#"><div class="circle-inner"><span class="szoveg">Demó</span></div></a>
+                </div>
+            </div>
+            <div class="col-xs-12 col-md-4">
+                <div class="responsive-circle kisKor3">
+                    <a href="#"><div class="circle-inner"><span class="szoveg">Interaktív program indítása</span></div></a>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </body>
 </html>
