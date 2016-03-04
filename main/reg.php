7c0c97a33a87c2ee9f58a1df4fd5be1dc1ea7571
Peter Tisoczki <Tisoczki.Peter.Mark@stud.u-szeged.hu>
2016-03-04 14:34:27 +0100

    reg.php is done

--- main/reg.php
+++ main/reg.php
@@ -10,6 +10,8 @@
     <link rel="stylesheet" type="text/css" href="css/component.css">
     <script src="../js/jquery.min.js"></script>
     <script src="js/jquery-ui.js"></script>
+    <script src="js/jquery.blockUI.js"></script>
+    <script src="js/reg.js"></script>
 </head>
 <body>
 <div id="page-wrapper">
@@ -42,63 +44,69 @@
                     <div class="line-magenta"></div>
                 </div>
                 <div class="content-right">
+                    <div class="kartya_ok" style="display:none">
+                        <h1>Kártya azonosítva</h1>
+                        <h2>A rendszer sikeresen azonosította kártyaszámát!</h2>
+                    </div>
                     <form style="margin-left: 5em">
                         <fieldset>
                             <legend>Regisztráció adatai</legend>
-                            <table class="reg">
+                            <table class="reg_kartya">
                                 <tr>
                                     <td>Plasztik kártya száma<span style="color: red;font-weight: bold;">*</span>:</td><td><input id="kartya" type="text" placeholder="Kezdjen el gépelni..." required autofocus></td>
                                 </tr>
+                            </table>
+                            <table class="reg" style="display: none;">
                                 <tr>
                                     <td colspan="2" align="center" style="background-color: rgba(1, 1, 1, 0.2)">Név</td>
                                 </tr>
                                 <tr>
-                                    <td>Vezetéknév<span style="color: red;font-weight: bold;">*</span>:</td><td><input id="vez_nev" type="text" placeholder="Példa" required></td>
+                                    <td>Vezetéknév<span style="color: red;font-weight: bold;">*</span>:</td><td><input id="vez_nev" type="text" maxlength="25" pattern="^[A-ZöüóőúéáűíÖÜÓŐÚÉÁŰÍ]{1}[a-zA-ZöüóőúéáűíÖÜÓŐÚÉÁŰÍ\.\- ]{1,24}" placeholder="Példa" required></td>
                                 </tr>
                                 <tr>
-                                    <td>Keresztnév<span style="color: red;font-weight: bold;">*</span>:</td><td><input id="k_nev" type="text" placeholder="Tamás" required></td>
+                                    <td>Keresztnév<span style="color: red;font-weight: bold;">*</span>:</td><td><input id="k_nev" type="text" maxlength="25" pattern="^[A-ZöüóőúéáűíÖÜÓŐÚÉÁŰÍ]{1}[a-zA-ZöüóőúéáűíÖÜÓŐÚÉÁŰÍ\.\- ]{1,24}" placeholder="Tamás" required></td>
                                 </tr>
                                 <tr>
-                                    <td>Előnév:</td><td><input id="e_nev" type="text" placeholder="Ifj."></td>
+                                    <td>Előnév:</td><td><input id="e_nev" type="text" maxlength="20" pattern="^[A-ZöüóőúéáűíÖÜÓŐÚÉÁŰÍ]{1}[\.\-a-zA-ZöüóőúéáűíÖÜÓŐÚÉÁŰÍ\s]{1,19}" placeholder="Ifj."></td>
                                 </tr>
                                 <tr>
                                     <td colspan="2" align="center" style="background-color: rgba(1, 1, 1, 0.2)">Elérhetőségek</td>
                                 </tr>
                                 <tr>
-                                    <td>E-mail cím<span style="color: red;font-weight: bold;">*</span>:</td><td><input id="e-mail" type="text" placeholder="valami@valami.hu" required></td>
+                                    <td>E-mail cím<span style="color: red;font-weight: bold;">*</span>:</td><td><input id="e-mail" type="text" maxlength="50" pattern="^[a-z0-9\.\-\_]{1,35}(\@)[a-z0-9\.\-\_]{1,20}(\.)[a-z0-9]{1,10}$" placeholder="valami@valami.hu" required></td>
                                 </tr>
                                 <tr>
-                                    <td>Telefonszám:</td><td><input id="t_szam" type="text" placeholder="30/123-4567"></td>
+                                    <td>Telefonszám:</td><td><input id="t_szam" type="text" maxlength="16" pattern="^(\+?[0-9]{1,3})([\/\-]?[0-9]{1,4}){1,3}" placeholder="30/123-4567"></td>
                                 </tr>
                                 <tr>
                                     <td colspan="2" align="center" style="background-color: rgba(1, 1, 1, 0.2)">Cég adatai</td>
                                 </tr>
                                 <tr>
-                                    <td>Vállalat/intézmény neve<span style="color: red;font-weight: bold;">*</span>:</td><td><input id="vall_neve" type="text" placeholder="Minta Bt." required></td>
+                                    <td>Vállalat/intézmény neve<span style="color: red;font-weight: bold;">*</span>:</td><td><input id="vall_neve" type="text" maxlength="40" pattern="[^\x22\x27\{\}\[\]\(\)]{3,40}" placeholder="Minta Bt." required></td>
                                 </tr>
                                 <tr>
-                                    <td>Vállalat/intézmény telephelye:</td><td><input id="vall_thely" type="text" placeholder="1111, Budapest, Ötletes u. 4."></td>
+                                    <td>Vállalat/intézmény telephelye:</td><td><input id="vall_thely" type="text" maxlength="50" pattern="[^\x22\x27\{\}\[\]\(\)]{3,50}" placeholder="1111, Budapest, Ötletes u. 4."></td>
                                 </tr>
                                 <tr>
                                     <td colspan="2" align="center" style="background-color: rgba(1, 1, 1, 0.2)">Lakhely</td>
                                 </tr>
                                 <tr>
-                                    <td>Lakóhely (város)<span style="color: red;font-weight: bold;">*</span>:</td><td><input id="lakhely_varos" type="text" placeholder="Szeged" required></td>
+                                    <td>Lakóhely (város)<span style="color: red;font-weight: bold;">*</span>:</td><td><input id="lakhely_varos" type="text" maxlength="25" pattern="^[A-ZöüóőúéáűíÖÜÓŐÚÉÁŰÍ]{1}[a-zA-ZöüóőúéáűíÖÜÓŐÚÉÁŰÍ\.\- ]{1,24}" placeholder="Szeged" required></td>
                                 </tr>
                                 <tr>
-                                    <td>Lakóhely (kerület/városrész):</td><td><input id="lakhely_vresz" type="text" placeholder="Makkosháza"></td>
+                                    <td>Lakóhely (kerület/városrész):</td><td><input id="lakhely_vresz" type="text" maxlength="25" pattern="^[A-ZöüóőúéáűíÖÜÓŐÚÉÁŰÍ]{1}[a-zA-ZöüóőúéáűíÖÜÓŐÚÉÁŰÍ\.\- ]{1,24}" placeholder="Makkosháza"></td>
                                 </tr>
                                 <tr>
                                     <td colspan="2" align="center" style="background-color: rgba(1, 1, 1, 0.2)">Válasszon saját felhasználónevet!</td>
                                 </tr>
                                 <tr>
-                                    <td>Felhasználónév<span style="color: red;font-weight: bold;">*</span>:</td><td><input id="user_n" type="text" placeholder="peldatomi" required></td>
+                                    <td>Felhasználónév<span style="color: red;font-weight: bold;">*</span>:</td><td><input id="user_n" type="text" maxlength="20" pattern="^[^0-9][a-zA-Z0-9\.\-\_]{1,19}" placeholder="peldatomi" required></td>
                                 </tr>
                                 <tr>
-                                    <td>Jelszó<span style="color: red;font-weight: bold;">*</span>:</td><td><input id="passw" type="password" placeholder="jelszo" required></td>
+                                    <td>Jelszó<span style="color: red;font-weight: bold;">*</span>:</td><td><input id="passw" type="password" maxlength="20" pattern="[^\x22\x27\{\}\[\]\(\)]{6,20}" placeholder="jelszo" required></td>
                                 </tr>
                                 <tr>
-                                    <td>Jelszó megerősítése<span style="color: red;font-weight: bold;">*</span>:</td><td><input id="passw_re" type="password" placeholder="jelszo" required></td>
+                                    <td>Jelszó megerősítése<span style="color: red;font-weight: bold;">*</span>:</td><td><input id="passw_re" type="password" maxlength="20" pattern="[^\x22\x27\{\}\[\]\(\)]{6,20}" placeholder="jelszo" required></td>
                                 </tr>
                                 <tr>
                                     <td>Adatkezelési szabályzat elfogadása<span style="color: red;font-weight: bold;">*</span>:</td><td><input id="adatk" type="checkbox" required></td>
